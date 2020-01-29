de2f0fe56646e3f07f174d78f82043e77aba2bf2
laschenkov.artem <laschenkov.artem@otr.ru>
2020-01-30 00:09:08 +0300

    create component

--- src/App.js
+++ src/App.js
@@ -1,11 +1,25 @@
 import React from 'react';
+import { Switch, Route } from "react-router-dom";
 import './App.css';
+
+import Navbar from "./components/Navbar";
+
+import Home from './pages/Home';
+import Rooms from './pages/Rooms';
+import SingleRooms from './pages/SingleRoom';
+import Error from './pages/Error';
 
 function App() {
   return (
-    <div className="App">
-        hello from app
-    </div>
+    <>
+      <Navbar />
+      <Switch>
+        <Route exact path="/" component={Home} />
+        <Route exact path="/rooms/" component={Rooms} />
+        <Route exact path="/rooms/:slug" component={SingleRoom} />
+        <Route component={Error} />
+      </Switch>
+    </>
   );
 }
 
---
+++ src/pages/Home.js
@@ -0,0 +1,19 @@
+import React from "react";
+import { Link } from "react-router-dom";
+
+export default const home = () => {
+  return (
+    <>
+      <Hero>
+        <Banner
+          title="luxurious rooms"
+          subtitle="deluxe rooms starting at $299"
+        >
+          <Link to="/rooms" className="btn-primary">
+            our rooms
+          </Link>
+        </Banner>
+      </Hero>
+    </>
+  );
+};
